8948cef792ff79dbc73e93bbab291554bba2c56e
Kames <info@kamescg.com>
2019-09-16 13:36:09 -0400

    Performing Quick Save

--- test/index.js
+++ test/index.js
@@ -1,8 +1,12 @@
 import { assert } from 'chai';
+import { Resolver } from 'did-resolver'
 import { verify } from '../src';
 
 describe('Functions Tests', () => {
   it('should test the verify function', () => {
     assert.typeOf(verify, 'function', 'Verify Exists');
+  });
+  it('should test the DIDResolver functions', () => {
+    assert.typeOf(Resolver, 'function', 'Constructor');
   });
 });
